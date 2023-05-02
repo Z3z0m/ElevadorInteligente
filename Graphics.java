d91d0438317965c88ff871e8dedbce3dda363d1e
Z3z0m <josepaulolfig@gmail.com>
2023-05-02 17:31:24 -0300

    V0,5
    #	modified:   Game.java
    #	modified:   Graphics.java
    #	modified:   Image/ElevatorClosed.png

--- Graphics.java
+++ Graphics.java
@@ -1,4 +1,15 @@
+// import java.awt.Graphics;
+import java.awt.Color;
+import javax.swing.JFrame;
 
-public class Graphics {
-		
+public class Graphics extends JFrame {
+    
+    public Graphics(){
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // RenderizadorPNG renderizador = new RenderizadorPNG("imagem.png");
+        // add(renderizador);
+        setBackground(Color.blue);
+        setVisible(true);
+    }    
 }
